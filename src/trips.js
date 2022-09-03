d348221a64023bf274428ffca24f76b76bd22540
diopmorfall <morfall.papydiop99@gmail.com>
2022-09-03 14:47:29 +0200

    trips data added

---
+++ src/trips.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        place: "Lecco",
+        country: "Italy",
+        googleMapsPosition: "https://www.google.it/maps/place/23900+Lecco+LC/@45.9362298,9.4449617,12.22z/data=!4m5!3m4!1s0x478402d32a793355:0x8f7caba70bc80602!8m2!3d45.8565698!4d9.3976704",
+        startDate: "2nd June 2022",
+        endDate: "6th June 2022",
+        description: `Lecco is a city on the shores of the beautiful Lake of Como (great for a walk), where the famous "The Betrothed"
+            novel was set.`,
+    },
+    {
+        id: 2,
+        place: "Bari",
+        country: "Italy",
+        googleMapsPosition: "https://www.google.it/maps/place/Bari+BA/@41.111356,16.8120402,12z/data=!3m1!4b1!4m5!3m4!1s0x1347e8f8f3078d6f:0x87254066a3908e1!8m2!3d41.1171432!4d16.8718715",
+        startDate: "30th June 2022",
+        endDate: "5th July 2022",
+        description: `Bari is one of the biggest city in south Italy, and is the regional capital of Apulia. Its old town, known as
+            "Bari vecchia" and "Borgo Murattiano" is very characteristic`,
+    },
+    {
+        id: 3,
+        place: "Polignano a Mare",
+        country: "Italy",
+        googleMapsPosition: "https://www.google.it/maps/place/70044+Polignano+a+Mare+BA/@40.9962086,17.2077652,15z/data=!3m1!4b1!4m5!3m4!1s0x1347cbeb9e80f67b:0x9bbe6b6e25a97601!8m2!3d40.9948946!4d17.22261",
+        startDate: "4th June 2022",
+        endDate: "4th June 2022",
+        description: `Polignano a Mare is a touristic city in the metropolitan area of Bari, and has many beautiful beaches, views and caves.
+            There's also a fish based fast food named "Pescaria", which is a great place for a lunch`,
+    }
+]
