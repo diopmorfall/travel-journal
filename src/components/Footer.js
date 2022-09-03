cbd2a698cfc0b61e6b8b034abd4927121f1217f3
diopmorfall <morfall.papydiop99@gmail.com>
2022-09-03 16:15:48 +0200

    Footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Footer(){
+    return(
+        <p className='footer'>
+            Wistfully built by <a href='https://github.com/diopmorfall'>Mor Fall Diop</a> ©
+        </p>
+    )
+}
