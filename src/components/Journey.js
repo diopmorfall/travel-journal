df6e7cebc47b994aa52e42c8558e0b6a928b9667
diopmorfall <morfall.papydiop99@gmail.com>
2022-09-03 15:28:29 +0200

    Props added to the Journey component

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import locationIcon from '../images/location.svg'
 
 export default function Journey(props){
     return(
         <section className='journey'>
-            <img className='journey-pic' src='' alt='Journey pic' />
+            <img className='journey-pic' src='' alt='Journey picture' />
             <div className='journey-details'>
                 <div className='journey-location'>
-                    <img src='location-icon' alt='Loc' />
-                    <span>JAPAN </span>   
-                    <a href='#'>View on Google Maps</a>
+                    <img src={locationIcon} alt='Location icon' />
+                    <span>{props.country.toUpperCase()}</span>   
+                    <a href={props.coordinates}>View on Google Maps</a>
                 </div>
-                <h1>Mount Fuji</h1>
-                <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
+                <h1>{props.location}</h1>
+                <h5>{props.startDate} - {props.endDate}</h5>
                 <p className='journey-description'>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
-                    Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                    {props.description}
                 </p>
             </div>
         </section>
--- src/travels.js
+++
@@ -1,5 +0,0 @@
-export default [
-    {
-        //todo: place, pic, description, dates, 
-    }
-]
